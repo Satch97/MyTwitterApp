7458a504322ff25f3dda1c41070fc15e5d2e0b5d
Satchin Campbell <satchinc@fb.com>
2016-06-27 19:21:05 -0700

    HomeTimeline Fragment implemented

---
+++ app/src/main/java/com/codepath/apps/mysimpletweets/fragments/HomeTimelineFragment.java
@@ -0,0 +1,55 @@
+package com.codepath.apps.mysimpletweets.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.codepath.apps.mysimpletweets.TwitterApplication;
+import com.codepath.apps.mysimpletweets.TwitterClient;
+import com.codepath.apps.mysimpletweets.models.Tweet;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
+
+/**
+ * Created by satchinc on 6/27/16.
+ */
+public class HomeTimelineFragment extends TweetsListFragment {
+    private TwitterClient client;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        client = TwitterApplication.getRestClient();
+        populateTimeline();
+    }
+
+    private void populateTimeline() {
+        client.getHomeTimeLine(new JsonHttpResponseHandler(){
+            //SUCCESS
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray json) {
+
+                //JSON here
+                //deserialize json
+                //create models
+                //load the model data into list view
+
+                addAll(Tweet.fromJSONArray(json));
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                Log.d("Debug", errorResponse.toString());
+            }
+
+            //FAILURE
+        });
+    }
+
+}
