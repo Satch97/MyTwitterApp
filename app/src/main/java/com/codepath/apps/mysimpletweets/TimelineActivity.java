f73eb9fd9618681b80493e0ac85852b79e0a8868
Satchin Campbell <satchinc@fb.com>
2016-06-28 15:26:24 -0700

    Fragments added

--- app/src/main/java/com/codepath/apps/mysimpletweets/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/mysimpletweets/TimelineActivity.java
@@ -1,8 +1,18 @@
 package com.codepath.apps.mysimpletweets;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.astuetz.PagerSlidingTabStrip;
+import com.codepath.apps.mysimpletweets.fragments.HomeTimelineFragment;
+import com.codepath.apps.mysimpletweets.fragments.MentionTimelineFragment;
 import com.codepath.apps.mysimpletweets.fragments.TweetsListFragment;
 
 public class TimelineActivity extends AppCompatActivity {
@@ -14,10 +24,66 @@ public class TimelineActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
+
+        //Get the viewpager
+        ViewPager vpPager = (ViewPager) findViewById(R.id.viewpager);
+        //set the viewpager adapter for the pager
+        vpPager.setAdapter(new TweetsPagerAdapter(getSupportFragmentManager()));
+        //find the sliding tabstrip
+        PagerSlidingTabStrip tabStrip = (PagerSlidingTabStrip) findViewById(R.id.tabs);
+        //attac hthe tabstrip to the view pager
+        tabStrip.setViewPager(vpPager);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_timeline, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onProfileView(MenuItem mi){
+        Intent i = new Intent(this, ProfileActivity.class);
+        startActivity(i);
     }
     //send api request to get the timeline json
     //fill the
 
+    public class TweetsPagerAdapter extends FragmentPagerAdapter{
+        final int  PAGE_COUNT = 2;
+        private String tabTitles[] = { "Home", "Mentions"};
+        public TweetsPagerAdapter(FragmentManager fm){
+            super(fm);
+        }
 
+        @Override
+        public Fragment getItem(int position) {
+            if(position==0){
+                return new HomeTimelineFragment();
+            }
+            else if (position==1){
+                return  new MentionTimelineFragment();
+            }
+            else
+                {
+                        return null;
+                }
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return tabTitles[position];
+        }
+
+        @Override
+        public int getCount() {
+            return tabTitles.length;
+        }
+    }
 
 }
---
+++ app/src/main/java/com/codepath/apps/mysimpletweets/fragments/UserTimelineFragment.java
@@ -0,0 +1,7 @@
+package com.codepath.apps.mysimpletweets.fragments;
+
+/**
+ * Created by satchinc on 6/28/16.
+ */
+public class UserTimelineFragment {
+}
